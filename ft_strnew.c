56acd0ac922883e1215d49f45b6fc1c356a5eb81
Joel Puronaho <90411699+jpuronah@users.noreply.github.com>
2022-01-23 11:25:56 +0200

    Trying stuff

---
+++ ft_strnew.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpuronah <jpuronah@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/07 14:39:08 by jpuronah          #+#    #+#             */
+/*   Updated: 2021/11/10 14:41:19 by jpuronah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnew(size_t size)
+{
+	size_t	i;
+	char	*str;
+
+	str = ft_memalloc(size + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		str[i] = '\0';
+		i++;
+	}
+	return (str);
+}
